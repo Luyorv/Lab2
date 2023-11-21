ad61bf2efe2051c9f54285f3ac7937e12e73bf40
Regina Victoria Luyo Manrique <reginalm@TTC.CAT>
2023-11-21 17:21:26 +0100

    PREGUNTA 1.4 Y 1.5 REALIZADAS

--- script.R
+++ script.R
@@ -14,3 +14,46 @@ title[1]
 list_texte <- xpathSApply(xml_base, "//a", xmlAttrs, 'href')
 
 class(list_texte)
+
+#pregunta 1.4
+
+library(dplyr)
+
+agg_tbl <- df_link %>% group_by(url,text) %>%  summarise(total_count=n(), .groups = 'drop')
+
+df_links2 <- agg_tbl %>% as.data.frame()
+
+
+#pregunta 1.5
+
+getstatuscode <- function(url){
+  #Sys.sleep(runif(1, min=1, max=1))
+  demo1 <- httr::GET(urlstandar(url))
+  res <- head(demo1)
+  return(res$status_code)
+}
+
+
+urlstandar <- function(url){
+  print(url)
+  if (startsWith(url,'//')) {
+    updated_url <- gsub(" ", "", paste('https:',url))
+    return(updated_url)
+  }
+  if(startsWith(url,'/')){
+    updated_url <- gsub(" ", "", paste('https://www.mediawiki.org',url))
+    return(updated_url)
+  }
+  if(grepl('#', url)){
+    return('https://www.mediawiki.org/wiki/MediaWiki')
+  }
+  return(url)
+}
+
+
+df_links3 <- df_links2
+
+df_links3$estatus <- sapply(df_links3$url,getstatuscode)
+
+df_links3$enlace <- sapply(df_links3$url,urlstandar)
+
